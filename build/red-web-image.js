0c54e89c26e6823baf89c8c3224d10016034f83a
redknot <redknotmiaoyuqiao@gmail.com>
2017-04-13 09:14:13 +0800

    Sobel

--- build/red-web-image.js
+++ build/red-web-image.js
@@ -1,4 +1,4 @@
-/**** 创建时间为:2017-04-13 03:51:56 ****/
+/**** 创建时间为:2017-04-13 09:10:21 ****/
 
 
 /**** GLContext.js ****/
@@ -246,13 +246,13 @@ var ImageLoad = {
     }
 };
 
-/**** ShaderToyRwi.js ****/
+/**** SimpleRwi.js ****/
 
 /**
- * 事实上，这是一个很有趣的类，你可以用它来轻松移植shaderToy上的shader
+ * 基本类
  */
 
-var ShaderToyRwi = {
+var SimpleRwi = {
 
     canvasId : null,
     FilterName : null,
@@ -272,7 +272,7 @@ var ShaderToyRwi = {
         this.glContext = glContextD.glContext;
 
         var vertex = Object.create(GLShader);
-        vertex.Init(this.glContext,"vertex",eval("vertex_" + FilterName));
+        vertex.Init(this.glContext,"vertex",eval("vertex_base"));
         var fragment = Object.create(GLShader);
         fragment.Init(this.glContext,"fragment",eval("fragment_" + FilterName));
 
@@ -310,8 +310,8 @@ var ShaderToyRwi = {
         /**
          * 设置Canvas实际大小
          */
-        var iResolution = new Float32Array([glContextD.docCanvas.width * 1.0, glContextD.docCanvas.height * 1.0, 1.0]);
-        this.draw.SetUniformVec3("iResolution",iResolution);
+        var resolution = new Float32Array([glContextD.docCanvas.width * 1.0, glContextD.docCanvas.height * 1.0, 1.0]);
+        this.draw.SetUniformVec3("resolution",resolution);
 
         /**
          * 缓存开始绘制的时间
@@ -321,8 +321,8 @@ var ShaderToyRwi = {
         /**
          * 传入iGlobalTime
          */
-        var iGlobalTime = (Date.parse(new Date()) / 1000) - this.START_TIME;
-        this.draw.SetUniformFloat("iGlobalTime",iGlobalTime);
+        var globalTime = (Date.parse(new Date()) / 1000) - this.START_TIME;
+        this.draw.SetUniformFloat("globalTime",globalTime);
     },
 
     /**
@@ -334,75 +334,5 @@ var ShaderToyRwi = {
         
         this.draw.SetUniformTexture(name, texture, unit);
         this.draw.DoDraw();
-    }
-};
-
-/**** SimpleRwi.js ****/
-
-/**
- * 封装了Program Redknot编写
- */
-var SimpleRwi = {
-
-    canvasId : null,
-    FilterName : null,
-
-    glContext : null,
-
-    draw : null,
-
-    Init : function(canvasId,FilterName,imageSrc)
-    {
-        this.canvasId = canvasId;
-        this.FilterName = FilterName;
-
-        var glContextD = Object.create(GLContext);
-        glContextD.Init(canvasId);
-        this.glContext = glContextD.glContext;
-
-        var vertex = Object.create(GLShader);
-        vertex.Init(this.glContext,"vertex",eval("vertex_" + FilterName));
-        var fragment = Object.create(GLShader);
-        fragment.Init(this.glContext,"fragment",eval("fragment_" + FilterName));
-
-        programV = Object.create(GLProgram);
-        programV.Init(this.glContext);
-        programV.AddShader(vertex);
-        programV.AddShader(fragment);
-        programV.LinkProgram();
-        programV.UseProgram();
-
-        this.draw = Object.create(Draw);
-        this.draw.Init(this.glContext,programV);
-
-        var data = new Float32Array([-1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0]);
-        this.draw.SetAttribute("pos",data,2);
-
-        var dataCoor = new Float32Array([0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0]);
-        this.draw.SetAttribute("texPos",dataCoor,2);
-
-        var indicesData = new Uint8Array([0, 1, 2, 0, 2, 3]);
-        this.draw.SetElementIndex(indicesData);
-
-        /**
-         * 创建贴图
-         */
-
-        var imageload = Object.create(ImageLoad);
-        ImageLoad.Init(imageSrc,"mainTexture",0,this);
-    },
-
-    ImgLoadCallBack: function (name,unit,img) {
-        var texture = Object.create(GLTexture);
-        texture.Init(this.glContext, img, unit);
-        
-        this.draw.SetUniformTexture(name, texture, unit);
-        this.draw.DoDraw();
-
-        this.OnLoadImage();
-    },
-
-    OnLoadImage : function(){
-
     }
 };
